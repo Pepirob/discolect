aeb6257583fabed8ebaa96c28538718dff27c852
Jose Luis <pepirob>
2023-02-18 13:54:15 +0100

    adding info to the profile view

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const { isLoggedIn } = require("../middleware/auth");
+const User = require("../models/User.model");
 
 router.get("/", isLoggedIn, async (req, res, next) => {
-  res.render("profile/my-profile.hbs");
+  try {
+    const userData = await User.findById(req.session.activeUser._id);
+    const date = new Date(userData.createdAt);
+    const registerDate = date.toLocaleDateString();
+
+    res.render("profile/my-profile.hbs", {
+      registerDate,
+      userData,
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
